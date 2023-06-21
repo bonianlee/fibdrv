6f0fb364c390aa3ff96335f6a78c4463270db80a
bonianlee <ccasdqwe@gamil.com>
2023-06-21 10:20:05 +0800

    Add time measurement for Fibonacci in user space

--- client.c
+++ client.c
@@ -3,16 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 
 #define FIB_DEV "/dev/fibonacci"
 
 int main()
 {
-    long long sz;
-
+    // long long sz;
     char buf[1];
-    char write_buf[] = "testing writing";
+    // char write_buf[] = "testing writing";
     int offset = 100; /* TODO: try test something bigger than the limit */
 
     int fd = open(FIB_DEV, O_RDWR);
@@ -21,28 +21,39 @@ int main()
         exit(1);
     }
 
-    for (int i = 0; i <= offset; i++) {
-        sz = write(fd, write_buf, strlen(write_buf));
-        printf("Writing to " FIB_DEV ", returned the sequence %lld\n", sz);
+    // for (int i = 0; i <= offset; i++) {
+    //     sz = write(fd, write_buf, strlen(write_buf));
+    //     printf("Writing to " FIB_DEV ", returned the sequence %lld\n", sz);
+    // }
+    FILE *fptr = fopen("UserTimeTaken.txt", "w");
+    if (!fptr) {
+        return 0;
     }
 
     for (int i = 0; i <= offset; i++) {
+        long long sz;
+        struct timespec t1, t2;
         lseek(fd, i, SEEK_SET);
+        clock_gettime(CLOCK_MONOTONIC, &t1);
         sz = read(fd, buf, 1);
+        clock_gettime(CLOCK_MONOTONIC, &t2);
+        long long ut =
+            ((t2.tv_sec * 1e9 + t2.tv_nsec) - (t1.tv_sec * 1e9 + t1.tv_nsec));
         printf("Reading from " FIB_DEV
                " at offset %d, returned the sequence "
-               "%lld.\n",
-               i, sz);
+               "%lld, taking time: %lld.\n",
+               i, sz, ut);
+        fprintf(fptr, "Userspace time taken at offset %d : %lld.\n", i, ut);
     }
 
-    for (int i = offset; i >= 0; i--) {
-        lseek(fd, i, SEEK_SET);
-        sz = read(fd, buf, 1);
-        printf("Reading from " FIB_DEV
-               " at offset %d, returned the sequence "
-               "%lld.\n",
-               i, sz);
-    }
+    // for (int i = offset; i >= 0; i--) {
+    //     lseek(fd, i, SEEK_SET);
+    //     sz = read(fd, buf, 1);
+    //     printf("Reading from " FIB_DEV
+    //            " at offset %d, returned the sequence "
+    //            "%lld.\n",
+    //            i, sz);
+    // }
 
     close(fd);
     return 0;
